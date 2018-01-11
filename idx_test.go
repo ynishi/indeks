2b3e09af6ebdc13520cea927c29291656b2ef3a2
Yutaka Nishimura <ytk.nishimura@gmail.com>
2018-01-12 01:04:01 +0900

    mod check and result test

--- idx_test.go
+++ idx_test.go
@@ -39,5 +39,23 @@ func TestCreateAction(t *testing.T) {
 }
 
 func TestCheckAction(t *testing.T) {
-
+	action := &Action{Idx: testIdx}
+	action.Idx = nil
+	action.TargetTime = time.Date(2018, 1, 13, 0, 0, 0, 0, time.UTC)
+	if action.Result != ResultUncheck {
+		t.Fatalf("Failed check unknown action\nhave: %v\nwant: %v", action.Result, ResultUncheck)
+	}
+	d := time.Date(2018, 1, 12, 0, 0, 0, 0, time.UTC)
+	action1 := Do(action, &d)
+	if action1.Result != ResultOK {
+		t.Fatalf("Failed check ok action\nhave: %v\nwant: %v", action1.Result, ResultOK)
+	}
+	d = time.Date(2018, 1, 14, 0, 0, 0, 0, time.UTC)
+	action2 := Do(action, &d)
+	if action2.Result != ResultNG {
+		t.Fatalf("Failed check ok action\nhave: %v\nwant: %v", action2.Result, ResultOK)
+	}
+	if !reflect.DeepEqual(action2.ActualTime, d) {
+		t.Fatalf("ActualTime is not Matched\nhave: %v\nwant: %v", action2.ActualTime, d)
+	}
 }
